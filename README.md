c1c72e2d1608b3b9218a000c0100dceddd2cb2d2
Sachin Kumar <98296481+Sachink003@users.noreply.github.com>
2024-05-17 10:52:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# The Tutorial Project - Solutions
+
+This repository contains solutions to the exercises in Patrick Massot's tutorial (aka "the tutorial project). 
+
+*Goal:* Prove a series of statements from undergraduate real analysis course, 
+culminating in a proof of the intermediate value theorem. 
---
+++ TheTutorialProject.lean
@@ -0,0 +1,13 @@
+-- This module serves as the root of the `TheTutorialProject` library.
+-- Import modules here that should be built as part of the library.
+import «TheTutorialProject».00FirstProofs
+import «TheTutorialProject».01EqualityRewriting
+import «TheTutorialProject».02IffIfAnd
+import «TheTutorialProject».03ForallOr
+import «TheTutorialProject».04Exists
+import «TheTutorialProject».05SequenceLimits
+import «TheTutorialProject».06SubSequences
+import «TheTutorialProject».07bisAbstractNegations
+import «TheTutorialProject».07FirstNegations
+import «TheTutorialProject».08LimitsNegation
+import «TheTutorialProject».09LimitsFinal
